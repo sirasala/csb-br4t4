a498ab077f64e41c4c20dcdcb81fa212f7cd3619
Code Sandbox <hello@codesandbox.io>
2021-12-01 11:31:52 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.0f573799986afd50273d2315e064a706.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ece152241e3c9fbc8002bf60346f7757",
+    "url": "https://sirasala.github.io/csb-br4t4/index.html"
+  },
+  {
+    "revision": "70cec629722c4017b449",
+    "url": "https://sirasala.github.io/csb-br4t4/static/js/2.8199e9b0.chunk.js"
+  },
+  {
+    "revision": "2aeb9da3686bb933e761",
+    "url": "https://sirasala.github.io/csb-br4t4/static/js/main.596b91e8.chunk.js"
+  },
+  {
+    "revision": "eb7d2f159505cc92a72c",
+    "url": "https://sirasala.github.io/csb-br4t4/static/js/runtime-main.031956ca.js"
+  }
+]);
